8df20498fb7cb215ac366756c459025560f867fc
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-05-11 00:56:48 +0300

    Created multi-page structure with react router

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/quiz">Quiz</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,7 @@
+export default function Quiz() {
+    return(
+        <div>
+            <h1>Quiz</h1>
+        </div>
+    )
+}
