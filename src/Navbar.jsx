9b22bf4f635b1bd9de90292481ca83e90810f400
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-05-12 14:25:07 +0300

    Created the desktop navigation bar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import './stylesheets/style.css'
+import './stylesheets/navbar.css';
+import logo from './images/bad-logo.png'
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <nav className="desktop-nav">
+      <ul className="desktop-nav-links">
+      <li className="desktop-nav-item">
+          <Link to="/"><img src={logo} alt="logo" id="logo" /></Link>
         </li>
-        <li>
-          <Link to="/about">About</Link>
+        <li className="desktop-nav-item">
+          <Link to="/" className="desktop-nav-text">Home</Link>
         </li>
-        <li>
-          <Link to="/quiz">Quiz</Link>
+        <li className="desktop-nav-item">
+          <Link to="/about" className="desktop-nav-text">About</Link>
+        </li>
+        <li className="desktop-nav-item">
+          <Link to="/quiz" className="desktop-nav-text">Quiz</Link>
         </li>
       </ul>
     </nav>
