339912ac6e1f56fb8d7aeaf3de101800e0cd88c7
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-05-16 13:00:19 +0300

    Made the simplest hamburger menu implementation

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import './stylesheets/style.css'
 import './stylesheets/navbar.css';
 import logo from './images/bad-logo.png'
 
-//TODO: Make the navbar respond to window size without need to refresh
-
 function Navbar() {
+
+  const [menuVisible, setMenuVisisble] = useState(false);
+
+  function toggleMenu() {
+    setMenuVisisble(!menuVisible);
+  }
+
   return (
-    <div>
+    <div className="sticky">
       <nav className="desktop-nav">
         <ul className="desktop-nav-links">
           <li className="desktop-nav-item">
             <Link to="/"><img src={logo} alt="logo" className="logo" /></Link>
           </li>
           <li className="desktop-nav-item">
-            <Link to="/" className="desktop-nav-text">Home</Link>
+            <Link to="/" className="nav-text">Home</Link>
           </li>
           <li className="desktop-nav-item">
-            <Link to="/about" className="desktop-nav-text">About</Link>
+            <Link to="/about" className="nav-text">About</Link>
           </li>
           <li className="desktop-nav-item">
-            <Link to="/quiz" className="desktop-nav-text">Quiz</Link>
+            <Link to="/quiz" className="nav-text">Quiz</Link>
           </li>
         </ul>
       </nav>
 
       <nav className="mobile-nav">
         <Link to="/" className="mobile-logo"><img src={logo} alt="logo" className="logo" /></Link>
-        <div class="hamburger">
+        <div class="hamburger" onClick={toggleMenu}>
             <svg viewBox="0 0 100 100" width="3.815rem" height="3.815rem" class="disable-pointer-effects">
                 <rect width="100" height="100" rx="8" fill="#000000"></rect>
                 <rect x ="25" y="30" width="50" height="6" rx="4" fill="#EEEEEE"></rect>
@@ -37,6 +42,23 @@ function Navbar() {
             </svg>
         </div>
       </nav>
+
+      {menuVisible && (
+        <div className="menu">
+          <ul className="mobile-menu-links">
+            <li className="mobile-menu-item">
+              <Link to="/" className="nav-text" onClick={toggleMenu}>Home</Link>
+            </li>
+            <li className="mobile-menu-item">
+              <Link to="/about" className="nav-text" onClick={toggleMenu}>About</Link>
+            </li>
+            <li className="mobile-menu-item">
+              <Link to="/quiz" className="nav-text" onClick={toggleMenu}>Quiz</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
             <main id='landing'>
                 <img src={backgroundImage} alt="" className="backgroundImage"/>
                 <div className="backgroundOverlay"></div>
-                <img src={phone} alt="" className="phoneImage"/>
+                <img src={phone} alt="social media apps" className="phoneImage"/>
                 <h1 className="heading">#BeatTheAlgorithm</h1>
             </main>
         </div>
