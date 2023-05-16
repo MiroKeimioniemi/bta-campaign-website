68ed012a89cf31fe6b01996321d1a09b08129b3f
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-05-16 19:26:45 +0300

    Finished the landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import '../stylesheets/style.css'
 import '../stylesheets/home.css'
 import backgroundImage from '../images/addicts.png'
@@ -11,7 +12,21 @@ export default function Home() {
                 <img src={backgroundImage} alt="" className="backgroundImage"/>
                 <div className="backgroundOverlay"></div>
                 <img src={phone} alt="social media apps" className="phoneImage"/>
-                <h1 className="heading">#BeatTheAlgorithm</h1>
+                <div className="heading-wrap">
+                    <h1 className="mobile-main-heading">
+                        #Beat
+                        <br/>
+                        &nbsp;&nbsp;The
+                        <br/>
+                        &nbsp;&nbsp;Algorithm
+                    </h1>
+                    <h1 className="desktop-main-heading">#BeatTheAlgorithm</h1>
+                </div>
+                <h2 className="landing-subheading">Challenge yourself to a month without social media</h2>
+                <p className="landing-text">Re-evaluate your relationship with social media. Who is winning, you or the algorithms?</p>
+                <Link to="/quiz"><button>
+                    Take Quiz
+                </button></Link>
             </main>
         </div>
     )
