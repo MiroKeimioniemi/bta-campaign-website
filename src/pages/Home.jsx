62514c9bb20613cb92adbf1c8a0421a46e284909
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-05-17 20:51:19 +0300

    Finished the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import '../stylesheets/home.css'
 import backgroundImage from '../images/addicts.png'
 import phone from '../images/cropped-phone.png'
 import lookingAtPhone from '../images/person-looking-at-phone.jpg'
+import socialDilemma from '../images/the-social-dilemma.png'
+import digitalMinimalism from '../images/digital-minimalism.png'
 
 export default function Home() {
     return(
@@ -35,6 +37,39 @@ export default function Home() {
                 <p className="clarification">Global daily average screen time*</p>
                 <p className="big-number">6h 58min</p>
             </section>
+
+            <section className="body-container">
+                <article className="why-article">
+                    <h2>Why take a break?</h2>
+                    <p>
+                        Social media is, in many ways, a very positive invention that connects you to all of humanity
+                        in a click of a button. However, therein lies its negative side as well. It spreads the good,
+                        the bad and the ugly ideas with unprecedented speed and volume, making it very easy to get
+                        entirely overwhelmed with all of the often negative content that usually is not really even
+                        relevant to you in any way. This makes many forget to pay attention to their local community
+                        and environment.
+                    </p>
+                    <p>
+                        Ideally, we can harness social media for all the good it brings by only using it intentionally
+                        and with moderation. However, this may be exceedingly difficult to do with predatory algorithms, 
+                        without reframing one's perspective first.
+                    </p>
+                    <p>
+                        Therefore, we invite you to re-evaluate your relationship with social media by taking a break and 
+                        only engaging with your local community for a month, after which you can carefully start re-introducing 
+                        the truly life-enhancing features of social media that you found yourself missing.
+                    </p>
+                </article>
+                <div className="video-container">
+                    <iframe width="1024" height="576" src="https://www.youtube-nocookie.com/embed/zKkjvkg37Jg" title="Social media survivor challenge" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="highlight-video"></iframe>
+                </div>
+                <h2>Further reading</h2>
+                <div className="further-reading">
+                    <a href="https://www.thesocialdilemma.com/"><img src={socialDilemma} alt="the social dilemma documentary" className="further-reading-image"/></a>
+                    <a href="https://calnewport.com/writing/"><img src={digitalMinimalism} alt="Cal Newport - Digital Minimalism book" className="further-reading-image"/></a>
+                </div>
+            </section>
+
         </div>
     )
 }
